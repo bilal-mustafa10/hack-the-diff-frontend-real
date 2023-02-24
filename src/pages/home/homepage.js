a2a4f95c49188f31e3a2092a7396f013a05376ec
bilal-mustafa10 <bilalmustafa041@icloud.com>
2023-02-24 10:51:59 +0000

    fix detached head

--- src/pages/home/homepage.js
+++ src/pages/home/homepage.js
@@ -34,7 +34,6 @@ const HomePage = () => {
 
     const Bard = () => {
         return(
-<<<<<<< HEAD
             <Card height={500} width={"100%"} margin = {15} backgroundColor={"#8149FF"} elevation={3} borderRadius={20}>
                 <Image
                     src={require('../../assets/images/camp.png')}
@@ -74,66 +73,6 @@ const HomePage = () => {
                     </form>
                 </Pane>
             </Card>
-
-
-=======
-            <>
-                <Pane display="flex" flexDirection="row">
-                    <Pane backgroundColor={"#71F5C4"} height={650} width={"100%"} display="flex" alignItems="center" justifyContent="center">
-                        <Card
-                            height={550}
-                            width={"100%"}
-                            margin = {15}
-                            backgroundColor={"#8149FF"}
-                            elevation={3}
-                            padding={0}
-                            borderRadius={20}
-                        >
-                            <Image 
-                                src={require('../../assets/images/camp.png')} 
-                                style={{ 
-                                    width:"100%", 
-                                    borderRadius: "10px",
-                                    height: "undefined",
-                                    aspectRatio: 1.5, // add this line
-                                    objectFit: "cover", // add this line
-                                    objectPosition: "center", // add this line
-                                }} 
-                                alt="Your image" 
-                            />
-                            <br />
-                            <br />
-                            <Pane padding={30}>
-                                <Text fontSize={30} fontFamily={"meridian"} color={"#FFFFFF"} fontWeight={"600"}>
-                                    Donate Now
-                                    <br />
-                                    <br />
-                                </Text>
-                                <Text fontSize={18} fontFamily={"meridian"} color={"#FFFFFF"} fontWeight={"300"}>
-                                    This holiday season, your gift will be matched to provide double the lifesaving assistance
-                                    <br />
-                                    <br />
-                                </Text>
-                                <form onSubmit={handleDonationSubmit}>
-                                    {/* Donation options buttons */}
-                                    {/* ... */}
-                                    {/* Donation amount buttons */}
-                                    {/* ... */}
-                                    {/* Other donation amount input */}
-                                    {/* ... */}
-                                    <Button appearance="primary" marginTop={10} width={"100%"} height={50} backgroundColor={"#1300c1"} borderRadius={8}>
-                                        Donate Now
-                                    </Button>
-                                </form>
-                            </Pane>
-                        </Card>
-                    </Pane>
-                </Pane>
-      
-            </>
-    
-            
->>>>>>> 06110dc5737cff7e4132c9a3968da70aebbe2231
         )
 
     }
@@ -217,23 +156,12 @@ const HomePage = () => {
             </Pane>
             {/* Second bottomhalf */}
             <Pane display="flex" flexDirection="row">
-<<<<<<< HEAD
                 <Pane backgroundColor={"#FFFFFF"} height={1200} width={"100%"} display="flex" alignItems="center" flexWrap="wrap" justifyContent="center" >
                     <Heading size={900} marginTop={22} marginLeft={22}>Active Campaigns</Heading>
                     <Pane
                         display="flex"
                         flexDirection="row"
                         padding={10}
-
-=======
-                <Pane backgroundColor={"#71F5C4"} height={650} width={"100%"} display="flex" alignItems="center" flexWrap="wrap" justifyContent="center" >
-                    <Heading size={900} marginTop={22} marginLeft={22}>Active Campaigns</Heading>
-                    <Pane
-                    display="flex"
-                    flexDirection="row"
-                    padding={10}
-                    
->>>>>>> 06110dc5737cff7e4132c9a3968da70aebbe2231
                     >
                         <Bard />
                         <Bard />
@@ -242,65 +170,13 @@ const HomePage = () => {
 
                     </Pane>
                     <Pane
-<<<<<<< HEAD
                         display="flex"
                         flexDirection="row"
                         padding={10}
-
                     >
                         <Bard />
                         <Bard />
                         <Bard />
-                        <Bard />
-
-                    </Pane>
-                    <br></br>
-                </Pane>
-            </Pane>
-            <Pane backgroundColor={"#71F5C4"} display="flex" flexDirection="column" padding={16}>
-
-                <Heading size={900} fontWeight={600} fontFamily="meridian" marginBottom={24}>
-                    FAQs
-                </Heading>
-
-                <Pane backgroundColor="#1300c1" padding={24} marginBottom={24}>
-                    <Heading size={700} fontWeight={600} fontFamily="meridian" color="#FFFFFF" marginBottom={16}>
-                        Why choose to support a mental health charity?
-                    </Heading>
-                    <Text color="#FFFFFF" marginBottom={16}>
-                        There are many reasons why someone might choose to support a mental health charity, including the fact that mental health affects everyone, there is often stigma and discrimination surrounding mental health issues, there is an increased need for mental health services, many people have personal experience with mental health issues, and supporting a charity can make a tangible difference in the lives of others.
-                    </Text>
-                </Pane>
-
-                <Pane backgroundColor="#1300c1" padding={24} marginBottom={24}>
-                    <Heading size={700} fontWeight={600} fontFamily="meridian" color="#FFFFFF" marginBottom={16}>
-                        Why choose to support NPT Mind?
-                    </Heading>
-                    <Text color="#FFFFFF" marginBottom={16}>
-                        NPT Mind is a local mental health charity that works for the better mental health of people in Neath Port Talbot by providing a range of services. If you want to fundraise for them, they will support you by providing you with fundraising materials, advice, and guidance. You can contact them by phone or email to get started.
-                    </Text>
-                </Pane>
-
-                <Pane backgroundColor="#1300c1" padding={24} marginBottom={24}>
-                    <Heading size={700} fontWeight={600} fontFamily="meridian" color="#FFFFFF" marginBottom={16}>
-                        Can I choose which projects my money funds?
-                    </Heading>
-                    <Text color="#FFFFFF">
-                        Yes, you can! You can choose to fundraise for a specific project or for the charity as a whole. If you want to fundraise for a specific project, you can select a specific project on the NPT Mind website and donate however much you want to.
-                    </Text>
-                </Pane>
-
-            </Pane>
-
-=======
-                    display="flex"
-                    flexDirection="row"
-                    padding={10}
-                    
-                    >
-                        <Bard />
-                        <Bard />
-                        <Bard />   
                         <Bard />
 
                     </Pane>
@@ -310,22 +186,22 @@ const HomePage = () => {
             {/* Third bottomhalf  FAQs*/}
             <Pane display="flex" flexDirection="row" >
                 <Pane backgroundColor={"#1300c1"} height={650} width={'100%'} display="flex" paddingTop={1}>
-                    
+
                     <Heading size={900} fontWeight={600} fontFamily={"meridian"} color={"#FFFFFF"}>
                             FAQs
-                    </Heading>            
-                    
+                    </Heading>
+
                     <Heading size={700} fontWeight={600} fontFamily={"meridian"} color={"#FFFFFF"}>
                         Why choose to support a mental health charity?
                     </Heading>
                     <Heading size={700} fontWeight={600} fontFamily={"meridian"} color={"#FFFFFF"}>
                         There are many reasons why someone might choose to support a mental health charity,
-                        including the fact that mental health affects everyone, there is often stigma and discrimination 
+                        including the fact that mental health affects everyone, there is often stigma and discrimination
                         surrounding mental health issues, there is an increased need for mental health services,
-                        many people have personal experience with mental health issues, and supporting a charity can make a 
+                        many people have personal experience with mental health issues, and supporting a charity can make a
                         tangible difference in the lives of others.
-                    </Heading>        
-                                
+                    </Heading>
+
                     <Heading size={700} fontWeight={600} fontFamily={"meridian"} color={"#FFFFFF"}>
                         Why choose to support a mental health charity?
                     </Heading>
@@ -333,8 +209,8 @@ const HomePage = () => {
 
 
                     <Heading size={700} fontWeight={600} fontFamily={"meridian"} color={"#FFFFFF"}>
-                        NPT Mind is a local mental health charity that works for the better mental health of people in Neath Port Talbot by providing a range of services6. 
-                        If you want to fundraise for them,they will support you by providing you with fundraising materials, advice and guidance7. You can contact them by 
+                        NPT Mind is a local mental health charity that works for the better mental health of people in Neath Port Talbot by providing a range of services6.
+                        If you want to fundraise for them,they will support you by providing you with fundraising materials, advice and guidance7. You can contact them by
                         phone or email to get started.
                     </Heading>
 
@@ -353,12 +229,6 @@ const HomePage = () => {
     );
 };
 
-<<<<<<< HEAD
-=======
-
 
 export default HomePage;
->>>>>>> 06110dc5737cff7e4132c9a3968da70aebbe2231
 
-
-export default HomePage;
